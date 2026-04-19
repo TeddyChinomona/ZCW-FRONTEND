80a31473ed0fadfeeafde79fb19317598d348f60
= <=>
2026-04-19 22:35:42 +0200

    fix: add allowedHosts configuration for preview and server in Vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['zcw-frontend-production.up.railway.app']
+  },
+  // If you see this error during local development/linking, 
+  // you can also add it to the server block:
+  server: {
+    allowedHosts: ['zcw-frontend-production.up.railway.app']
+  }
 })
